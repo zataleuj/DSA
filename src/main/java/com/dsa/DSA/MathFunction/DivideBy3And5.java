ddea6221fc6bb61d5a6eb249b9fe3f6c3d908ee5
zataleuj <zataleuj@gmail.com>
2025-12-03 23:45:05 +0530

    Math function added

---
+++ src/main/java/com/dsa/DSA/MathFunction/DivideBy3And5.java
@@ -0,0 +1,13 @@
+package com.dsa.DSA.MathFunction;
+
+public class DivideBy3And5 {
+    public static void main(String[] args) {
+        int number=20;
+        int divByThree=number/3;
+        int divByFive=number/5;
+        int mul=number/(3*5);
+        int result=divByThree+divByFive-mul;
+        System.out.println(result);
+
+    }
+}
